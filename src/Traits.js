30d16c1585a4ffd949c9c0f21694c3ee89a7d559
TesteBot-crypto <testerbot@tuta.io>
2021-08-11 10:33:38 -0500

    v1.0.1 with styling on Botero Details

--- src/Traits.js
+++ src/Traits.js
@@ -10,10 +10,13 @@ const Traits = () => {
   const { botero } = location.state;
  console.log(botero)
   return (
+    <React.Fragment>
+      <Link to="/gallery" className="link"><button className="btn3">Back</button></Link>
       <article className="infocard">
-        <img src={ botero[0].image } alt="NFTinfo" className="nftimginfo"/>
-        <h2>{ botero[1].description }</h2>
+        <img src={ botero[0].image } alt="NFTinfo" className="nftimg"/>
+        <div className="info"><h1>Description:</h1><h3>{ botero[1].description }</h3></div>
       </article>
+    </React.Fragment>
   );
 
 }
